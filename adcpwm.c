aa9b793fb821dced8dfaac435573a95b5c65c9f3
mateusz59861 <mateusz-karpik@wp.pl>
2016-10-24 23:28:21 +0200

    heh

--- adcpwm.c
+++ adcpwm.c
@@ -97,7 +97,7 @@ int main (int argc, char *argv [])
     for (;;)
   	{   
 		printf("------------------------------------------------------------- \n");
-        pwmWrite (1, myAnalogRead(spiChannel,channelConfig,0)/2) ;
+        pwmWrite (1, myAnalogRead(spiChannel,channelConfig,1)) ;
 		if(analogChannel>0)
 		{
 			printf("MCP3008(CE%d,%s): analogChannel %d = %d\n",spiChannel,(channelConfig==CHAN_CONFIG_SINGLE)
